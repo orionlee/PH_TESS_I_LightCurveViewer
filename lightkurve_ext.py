64103756ead60121dd1cebbb3e45bd87e1a9d823
orionlee <orionlee@yahoo.com>
2019-10-20 13:05:00 -0700

    Extract common search, download and cache into a helper

---
+++ lightkurve_ext.py
@@ -0,0 +1,111 @@
+import os
+import logging
+import json
+
+import lightkurve as lk
+
+log = logging.getLogger(__name__)
+
+def download_lightcurvefiles(target, mission=('Kepler', 'K2', 'TESS'), download_dir=None, use_cache='yes'):
+    '''
+    Wraps `lightkurve.serach.search_lightcurvefile()` and the subsequent `lightkurve.serach.SearchResult.download_all()` calls,
+    with the option of caching, so that for a given search, if the the result has been downloaded, the cache will be used.
+    
+    The parameters all propagate to the underlying `search_lightcurvefile()` and `download_all()` calls. The lone exception
+    is `use_cache`.
+    
+    Parameters
+    ----------
+    use_cache : str, must be one of 'yes', 'no'
+        'yes': cache will be used when available. 'no': cache will not be used.\n
+        OPEN: support an  option of 'fallback': cache will be used when offline.\n
+        OPEN: for now, actual download lightcurve cache will still be used if available irrespective of the settings.
+        
+    Returns
+    -------
+    collection : `~lightkurve.collections.Collection` object
+        Returns a `~lightkurve.collections.LightCurveFileCollection`
+        containing all lightcurve files that match the criteria
+    '''
+
+    if 'no' == use_cache:
+        return _search_and_cache(target, mission, download_dir)
+    elif 'yes' == use_cache:
+        result_file_ids = _load_from_cache_if_any(target, mission, download_dir)
+        if result_file_ids is not None:
+            result_files = list(map(lambda e: f"{download_dir}/mastDownload/{e}", result_file_ids))
+            return lk.collections.LightCurveFileCollection(list(map(lambda f: lk.open(f), result_files)))
+        else:
+            return _search_and_cache(target, mission, download_dir)
+    else:
+        raise ValueError('invalid value for argument use_cache')
+
+
+def _search_and_cache(target, mission, download_dir):
+        search_res = lk.search.search_lightcurvefile(target=target, mission=mission)
+        _cache_search_result_product_identifiers(search_res, download_dir, target, mission)
+        return search_res.download_all(quality_bitmask='default', download_dir=download_dir)
+
+def _load_from_cache_if_any(target, mission, download_dir):
+    key = _get_cache_key(target, mission)
+    return _load_search_result_product_identifiers(download_dir, key)
+    
+    
+def _cache_search_result_product_identifiers(search_res, download_dir, target, mission):
+    key = _get_cache_key(target, mission)
+    identifiers = _to_product_identifiers(search_res)
+    _save_search_result_product_identifiers(identifiers, download_dir, key)
+    return key
+
+def _get_search_result_cache_dir(download_dir):
+    # TODO: handle download_dir is None (defaults)
+    cache_dir = f"{download_dir}/mastQueries"
+    
+    if os.path.isdir(cache_dir):
+        return cache_dir
+    else: 
+        # if it doesn't exist, make a new cache directory
+        try:
+            os.mkdir(cache_dir)
+        # downloads locally if OS error occurs
+        except OSError:
+            log.warning('Warning: unable to create {}. '
+                        'Cache MAST query results to the current '
+                        'working directory instead.'.format(cache_dir))
+            cache_dir = '.'
+    return cache_dir
+
+def _get_cache_key(target, mission):
+    #TODO: handle cases the gerneated key is not a valid filename
+    return f"{target}_{mission}_ids"
+
+
+def _to_product_identifiers(search_res):
+    '''
+    Returns
+    -------
+    A list of str, constructed from `(obs_collection, obs_id, productFilename)` tuples, that can 
+    identify cached lightcurve file,s if any.
+    '''
+    return list(map(lambda e: e['obs_collection'] +  '/' + e['obs_id'] + '/' + e['productFilename']
+         , search_res.table))
+
+def _save_search_result_product_identifiers(identifiers, download_dir, key):    
+    resolved_cahce_dir = _get_search_result_cache_dir(download_dir)
+    filepath = f"{resolved_cahce_dir}/{key}.json"
+    fp = open(filepath, 'w+')
+    json.dump(identifiers, fp)
+    return filepath
+
+def _load_search_result_product_identifiers(download_dir, key):    
+    resolved_cahce_dir = _get_search_result_cache_dir(download_dir)
+    filepath = f"{resolved_cahce_dir}/{key}.json"
+    try:
+        fp = open(filepath, 'r')
+        return json.load(fp)
+    except OSError as err:
+        # errno == 2: file not found, typical case of cache miss
+        # errno != 2: unexpected error, log a warning
+        if err.errno != 2:
+            log.warning('Unexpected OSError in retrieving cached search result: ' + err)
+        return None
