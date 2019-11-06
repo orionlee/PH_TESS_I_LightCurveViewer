a48db6fe3efdbf57df9b5f2fc1b1dc6f0d1c1a74
orionlee <orionlee@yahoo.com>
2019-11-06 09:23:27 -0800

    plot improvement: handle time gap in plot_all()

--- tic_plot.py
+++ tic_plot.py
@@ -83,6 +83,14 @@ def add_relative_time(lc):
     lc.time_rel = lc.time - t_start
     return lc.time_rel
 
+def mask_gap(x, y, min_x_diff):
+    """
+    Help to plot graphs with gaps in the data, so that straight line won't be draw to fill the gap.
+    Return a masked y that can be passed to pyplot.plot() that can show the gap.
+    """
+    x_diff = np.diff(x, prepend=-min_x_diff)
+    return np.ma.masked_where(x_diff > min_x_diff, y)
+
 def plot_n_annotate_lcf(lcf, ax, xmin=None, xmax=None, t0=None, t_start=None, t_end=None, moving_avg_window='30min', lc_tweak_fn=None):
     if lcf == None:
         print("Warning: lcf is None. Plot skipped")
@@ -169,8 +177,10 @@ def plot_all(lcf_coll, moving_avg_window=None, lc_tweak_fn=None, ax_fn=None, use
                  
         # convert to dataframe to add moving average
         if moving_avg_window is not None: 
-            df = add_flux_moving_average(lc, moving_avg_window)    
-            ax.plot(lc.time, df['flux_mavg'], c='black', label=f"Moving average ({moving_avg_window})")
+            df = add_flux_moving_average(lc, moving_avg_window)
+            # mask_gap: if there is a gap larger than 2 hours, 
+            # show the gap rather than trying to fill the gap with a straight line.     
+            ax.plot(lc.time, mask_gap(lc.time, df['flux_mavg'], 2/24), c='black', label=f"Moving average ({moving_avg_window})")
                  
         ax.set_title(f"{lcf_coll[0].PDCSAP_FLUX.label}, sectors {lcf_coll[i].header()['SECTOR']}")
 #         ax.legend()
