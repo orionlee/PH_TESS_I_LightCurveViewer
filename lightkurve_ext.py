90008805e8b7618d8b4d05d1104e78349176a4cf
orionlee <orionlee@yahoo.com>
2021-04-23 17:17:44 -0700

    lkv2: fix get_transit_times_in_lc use case

--- lightkurve_ext.py
+++ lightkurve_ext.py
@@ -225,6 +225,8 @@ def get_segment_times_idx(times, break_tolerance=5):
 
     The logic is adapted from `LightCurve.flatten`
     """
+    if hasattr(times, 'value'):  # convert astropy Time to raw values if needed
+        times = times.value
     dt = times[1:] - times[0:-1]
     with warnings.catch_warnings():  # Ignore warnings due to NaNs
         warnings.simplefilter("ignore", RuntimeWarning)
@@ -234,6 +236,8 @@ def get_segment_times_idx(times, break_tolerance=5):
     return (low, high)
 
 def get_segment_times(times, **kwargs):
+    if hasattr(times, 'value'):  # convert astropy Time to raw values if needed
+        times = times.value
     low, high = get_segment_times_idx(times, **kwargs)
     # add a small 1e-10 to end so that the end time is exclusive (follow convention in range)
     return [(times[l], times[h-1] + 1e-10) for l, h in zip(low, high)]
