d0686cf952caa16bdd16c53d605456e57c3161a2
orionlee <orionlee@yahoo.com>
2022-05-01 19:32:09 -0700

    centroid change plot

--- tic_plot.py
+++ tic_plot.py
@@ -1267,6 +1267,96 @@ def plot_skip_data_gap(lc, wspace=0.2, figsize=(16, 4), data_gap_min_days=10, **
     return axs
 
 
+def plot_centroids(lc, transit_time=None, window=None):
+
+    # based on:
+    # https://github.com/noraeisner/PH_Coffee_Chat/blob/4a723030ed80eeabdfb0eca49d948b97d61e35f6/False%20Positive/False%20positives%20-%20(4)%20centroid-position.ipynb
+
+    # bin the data, skip it to speed up the plot due to astropy performance regression
+    # lc_bin = lc.bin(7/60/24)
+    lc_bin = lc
+
+    # generate a mask so that we only see the times around the transit event
+    if (window is None) or (transit_time is None):
+        transit_mask = lc_bin.time.value > 0
+    else:
+        transit_mask = (lc_bin.time.value > transit_time - window) & (lc_bin.time.value < transit_time + window)
+
+    # make a plot with three panels so that we can see the lightcurve and the centroid positions
+    with plt.style.context(lk.MPLSTYLE):
+        fig, ax = plt.subplots(3, 1, figsize=(8, 5), sharex=True)
+
+    # plot the lightcurve in the top panel (in orange)
+    ax[0].plot(
+        lc_bin.time.value[transit_mask], lc_bin.sap_flux.value[transit_mask], color="darkorange", lw=0, marker=".", ms=3
+    )
+
+    # plot the centroid motions in the column direction in the middle panel
+    ax[1].plot(
+        lc_bin.time.value[transit_mask],
+        lc_bin.mom_centr1.value[transit_mask] - np.nanmean(lc_bin.mom_centr1.value[transit_mask]),
+        color="black",
+        lw=0,
+        marker=".",
+        ms=2,
+        alpha=0.5,
+    )
+    ax[1].plot(
+        lc_bin.time.value[transit_mask],
+        lc_bin.pos_corr1.value[transit_mask] - np.nanmean(lc_bin.pos_corr1.value[transit_mask]),
+        color="red",
+        lw=0,
+        marker=".",
+        ms=2,
+        alpha=0.5,
+    )
+
+    # plot the centroid motions in the row direction in the middle panel
+    ax[2].plot(
+        lc_bin.time.value[transit_mask],
+        lc_bin.mom_centr2.value[transit_mask] - np.nanmedian(lc_bin.mom_centr2.value[transit_mask]),
+        color="black",
+        lw=0,
+        marker=".",
+        ms=2,
+        alpha=0.5,
+        label="Brightness motion",
+    )
+    ax[2].plot(
+        lc_bin.time.value[transit_mask],
+        lc_bin.pos_corr2.value[transit_mask] - np.nanmedian(lc_bin.pos_corr2.value[transit_mask]),
+        color="red",
+        lw=0,
+        marker=".",
+        ms=2,
+        alpha=0.5,
+        label="Satellite motion",
+    )
+
+    if transit_time != None:
+        # draw a vertical line at the time of the transit event
+        ax[0].axvline(transit_time, color="grey", zorder=-1)
+        ax[1].axvline(transit_time, color="grey", zorder=-1)
+        ax[2].axvline(transit_time, color="grey", zorder=-1)
+
+    # label the axes
+    ax[0].set_ylabel("Flux")
+    ax[1].set_ylabel("Column")
+    ax[2].set_ylabel("Row")
+    plt.xlabel(f"Time ({lc.time.format.upper()})")
+
+    ax[2].xaxis.set_major_formatter(FormatStrFormatter("%.2f"))
+
+    if (window is not None) and (transit_time is not None):
+        plt.xlim(transit_time - window, transit_time + window)
+
+    plt.tight_layout()
+    plt.legend()
+    plt.show()
+
+    return ax
+
+
 def scatter_centroids(
     lcf,
     fig=None,
