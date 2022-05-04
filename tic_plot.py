bc2bc7357f4a47a8f49c30dcd458ede354dbac8a
orionlee <orionlee@yahoo.com>
2022-05-04 09:23:51 -0700

    per-pixel plot (LATTE variant tailored for transits)

--- tic_plot.py
+++ tic_plot.py
@@ -2087,7 +2087,7 @@ def plot_with_aperture_n_background(
     return ax
 
 
-def plot_in_out_diff(tpf, epoch, epoch_half_duration=0.25, oot_outer_relative=0.5, oot_inner_relative=0.3, plot_lc=True):
+def plot_in_out_diff(tpf, epoch, transit_half_duration=0.25, oot_outer_relative=0.5, oot_inner_relative=0.3, plot_lc=True):
     """
     Plot the in transit average flux and the out of transit average flux and compare the two (difference image).
     """
@@ -2112,7 +2112,7 @@ def plot_in_out_diff(tpf, epoch, epoch_half_duration=0.25, oot_outer_relative=0.
         T0 = T0_list[idx]  # the time of the transit-like event
         t = t_list[idx]  # the time array
 
-        intr = abs(T0 - t) < epoch_half_duration  # create a mask of the in transit times
+        intr = abs(T0 - t) < transit_half_duration  # create a mask of the in transit times
         oot = (abs(T0 - t) < oot_outer_relative) * (
             abs(T0 - t) < oot_inner_relative
         )  # create a mask of the out of transit times
@@ -2159,8 +2159,158 @@ def plot_in_out_diff(tpf, epoch, epoch_half_duration=0.25, oot_outer_relative=0.
         ax = plt.figure(figsize=(6, 3)).gca()
         ax = lc.scatter(ax=ax)
         ax.axvline(T0, color="red", ymax=0.15, linestyle="--", label="epoch")
-        ax.axvspan(T0 - epoch_half_duration, T0 + epoch_half_duration, facecolor="red", alpha=0.3, label="In Transit")
+        ax.axvspan(T0 - transit_half_duration, T0 + transit_half_duration, facecolor="red", alpha=0.3, label="In Transit")
         ax.axvspan(T0 - oot_outer_relative, T0 - oot_inner_relative, facecolor="green", alpha=0.3, label="Out of Transit")
         # no label to avoid double legend
         ax.axvspan(T0 + oot_inner_relative, T0 + oot_outer_relative, facecolor="green", alpha=0.3)
         ax.legend()
+
+
+def plot_pixel_level_LC(tpf, epoch, transit_half_duration=0.25, oot_outer_relative=0.5, oot_inner_relative=0.3):
+    """
+    Plot the LC for each pixel around the time of the transit like event.
+    Each LC is fitted with a 3 order polynomial in order to flatten.
+
+    Returns
+    -------
+        Plot of the normalised LC for each pixel around the time of the transit like event.
+        The pixel background colour represents the average flux.
+        The time of the transit is highlighted in red/gold for each pixel LC.
+    """
+
+    # based on plot_pixel_level_LC() in
+    # https://github.com/noraeisner/PH_Coffee_Chat/blob/4a723030ed80eeabdfb0eca49d948b97d61e35f6/False%20Positive/False%20positives%20-%20(3)%20pixel-level-lightcurve-plot.ipynb
+
+    transit_list = [epoch]
+    t_list = [tpf.time.value]
+    tpf_list = [tpf.flux.value]
+    bkg_list = [np.nanmean(tpf.flux.value, axis=0)]
+    arrshape_list = [tpf.flux.shape]
+
+    # loop through the transits and make plot for each ( only the first is currently displayed in the pdf report)
+    plot_half_width = oot_outer_relative * 1.25
+    for idx, X1_original in enumerate(tpf_list):
+
+        bkg = np.flip(bkg_list[idx], axis=0)
+        arrshape = arrshape_list[idx]
+        peak = transit_list[idx]
+        tpf = tpf_list[idx]
+
+        s = X1_original.shape
+        X1 = X1_original.reshape(s[0], s[1] * s[2])
+
+        T0 = transit_list[idx]  # the time of the transit-like event
+        t = t_list[idx]  # the time array
+
+        intr = abs(T0 - t) < transit_half_duration  # create a mask of the in transit times
+        oot = (abs(T0 - t) < oot_outer_relative) * (
+            abs(T0 - t) < oot_inner_relative
+        )  # create a mask of the out of transit times
+
+        fig, ax = plt.subplots(
+            arrshape[1], arrshape[2], sharex=True, sharey=False, gridspec_kw={"hspace": 0, "wspace": 0}, figsize=(5.5, 5.5)
+        )
+
+        plt.tight_layout()
+
+        # see if the background of this plot can be the average pixel flux (if there are too many nans this will fail and the background will just be black which is also okay)
+        try:
+            color = plt.cm.viridis(np.linspace(0, 1, int(np.nanmax(bkg)) - int(np.nanmin(bkg)) + 1))
+            simplebkg = False
+        except:
+            simplebkg = True
+
+        for i in range(0, arrshape[1]):
+            ii = arrshape[1] - 1 - i  # we want to plot this such that the pixels increase from left to right and bottom to top
+
+            for j in range(0, arrshape[2]):
+
+                apmask = np.zeros(arrshape[1:], dtype=np.int)
+                apmask[i, j] = 1
+                apmask = apmask.astype(bool)
+
+                flux = X1[:, apmask.flatten()].sum(axis=1)
+
+                m = np.nanmedian(flux[oot])
+
+                normalizedflux = flux / m
+
+                # bin the data
+                f1 = normalizedflux
+                time = t
+
+                binfac = 7
+
+                N = len(time)
+                n = int(np.floor(N / binfac) * binfac)
+                X = np.zeros((2, n))
+                X[0, :] = time[:n]
+                X[1, :] = f1[:n]
+                Xb = rebin(X, (2, int(n / binfac)))
+
+                # binned data
+                time_binned = np.array(Xb[0])
+                flux_binned = np.array(Xb[1])
+
+                # create a mask that only looks at the times cut around the transit-event
+                timemask = (time_binned < peak + plot_half_width) & (time_binned > peak - plot_half_width)
+
+                time_binned = time_binned[timemask]
+                flux_binned = flux_binned[timemask]
+
+                # ----------
+                # fit a spline to the cut-out of each pixel LC in order to flatten it
+                p = np.poly1d(np.polyfit(time_binned, flux_binned, 3))
+                flux_binned = flux_binned / p(time_binned)
+                # ----------
+
+                intr = abs(peak - time_binned) < 0.1
+
+                if simplebkg == True:
+                    ax[ii, j].set_facecolor(color="k")
+                    linecolor = "w"
+                    transitcolor = "gold"
+                else:
+                    ax[ii, j].set_facecolor(color=color[int(bkg[ii, j]) - int(np.nanmin(bkg))])
+
+                    if int(bkg[ii, j]) - abs(int(np.nanmin(bkg))) > ((np.nanmax(bkg)) - abs(int(np.nanmin(bkg)))) / 2:
+                        linecolor = "k"
+                        transitcolor = "orangered"
+                    else:
+                        linecolor = "w"
+                        transitcolor = "gold"
+
+                ax[ii, j].plot(time_binned, flux_binned, color=linecolor, marker=".", markersize=1, lw=0)
+                ax[ii, j].plot(time_binned[intr], flux_binned[intr], color=transitcolor, marker=".", markersize=1, lw=0)
+
+                # get rid of ticks and ticklabels
+                ax[ii, j].set_yticklabels([])
+                ax[ii, j].set_xticklabels([])
+                ax[ii, j].set_xticks([])
+                ax[ii, j].set_yticks([])
+
+        # ------------------
+
+        # print("done.\n")
+        # ------------------
+
+        # label the pixels
+
+        fig.text(0.5, 0.01, "column (pixel)", ha="center", fontsize=13)
+        fig.text(0.01, 0.5, "row (pixel)", va="center", rotation="vertical", fontsize=13)
+
+        # - - - - - - - - - -
+
+        plt.subplots_adjust(top=0.95, right=0.99, bottom=0.04, left=0.04)
+
+        plt.suptitle(r"T0 = {} $\pm$ {:.2f} d".format(peak, plot_half_width), y=0.98, fontsize=12)
+        plt.xlim(peak - plot_half_width, peak + plot_half_width)
+        plt.show()
+
+
+def rebin(arr, new_shape):
+    """
+    function used to rebin the data
+    """
+    shape = (new_shape[0], arr.shape[0] // new_shape[0], new_shape[1], arr.shape[1] // new_shape[1])
+    return arr.reshape(shape).mean(-1).mean(1)
