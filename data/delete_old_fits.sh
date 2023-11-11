c88ecf5ef00296b62ccac02e4114f05879192e37
orionlee <orionlee@yahoo.com>
2023-11-11 11:36:34 -0800

    delete fits util: delete my downloaded DV files too

--- data/delete_old_fits.sh
+++ data/delete_old_fits.sh
@@ -1,3 +1,13 @@
 #/bin/sh
 
+set -e
+
+# Ideally want touch use -atime (last access time), 
+# insetad of -mtime  (modified time)
+# but -atime does notify seem touch work (no file selected)
 find . -type f -name "*.fits" -mtime +365 -delete
+
+# Delete download DV files
+cd ~/Downloads
+find . -type f -name "tess*.pdf" -mtime +30 -delete
+cd -
