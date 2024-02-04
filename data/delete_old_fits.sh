cfe0396d30607e7d544479fb9ad262a7d63160dc
orionlee <orionlee@yahoo.com>
2024-02-03 21:55:08 -0800

    delete old fits util: include eleanor Tesscut

--- data/delete_old_fits.sh
+++ data/delete_old_fits.sh
@@ -7,6 +7,13 @@ set -e
 # but -atime does notify seem touch work (no file selected)
 find . -type f -name "*.fits" -mtime +365 -delete
 
+# tesscut (from Eleanor)
+find tesscut -type f -name "*.fits" -mtime +30 -delete
+
+# Consider to delete targetpixelfiles more aggressively
+# find . -type f -name "*_tp.fits" -mtime +30 -delete
+
+
 # Delete download DV files
 cd ~/Downloads
 find . -type f -name "tess*.pdf" -mtime +30 -delete
