0fe2a11c42dc4a992538a6ba3ccdec9a82c77c96
orionlee <orionlee@yahoo.com>
2024-03-11 21:33:37 -0700

    tess_dv_fast webapp gcloud: securityh - disable interactive debugger in public-facing env

--- tess_dv_fast_webapp_gcloud/main.py
+++ tess_dv_fast_webapp_gcloud/main.py
@@ -8,7 +8,8 @@ import tess_dv_fast_webapp
 app = tess_dv_fast_webapp.app  # Google Cloud Run requires `app` attribute in main
 if __name__ == "__main__":
     app.run(
-        debug=True, 
-        host=os.environ.get("TESS_TCES_HOST", "0.0.0.0"), 
-        port=int(os.environ.get("PORT", 8080))
-        )
+        debug=True,
+        use_evalex=False,  # disable werkzeug's web-based  interactive debugger on error page
+        host=os.environ.get("TESS_TCES_HOST", "0.0.0.0"),
+        port=int(os.environ.get("PORT", 8080)),
+    )
