2d0a8f8e9c9a0637fc8965f6f8590fce930fc71b
orionlee <orionlee@yahoo.com>
2024-03-12 20:02:52 -0700

    tess_dv_fast gcloud: disable hot reload

--- tess_dv_fast_webapp_gcloud/main.py
+++ tess_dv_fast_webapp_gcloud/main.py
@@ -9,6 +9,7 @@ app = tess_dv_fast_webapp.app  # Google Cloud Run requires `app` attribute in ma
 if __name__ == "__main__":
     app.run(
         debug=True,
+        use_reloader=False,  # disable hot reload and avoid the extra watcher process
         use_evalex=False,  # disable werkzeug's web-based  interactive debugger on error page
         host=os.environ.get("TESS_TCES_HOST", "0.0.0.0"),
         port=int(os.environ.get("PORT", 8080)),
