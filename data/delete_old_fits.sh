c3be00d42c5fba1a5d14e1608c7f84d49fef683d
orionlee <orionlee@yahoo.com>
2024-11-27 09:08:10 -0800

    Prune old cached fits: more aggressive

--- data/delete_old_fits.sh
+++ data/delete_old_fits.sh
@@ -2,19 +2,26 @@
 
 set -e
 
+echo "Disk usage before cleanup"
+df -h .
+du -h -s mastDownload tesscut .
+
 # Ideally want touch use -atime (last access time), 
 # insetad of -mtime  (modified time)
 # but -atime does notify seem touch work (no file selected)
-find . -type f -name "*.fits" -mtime +365 -delete
+find . -type f -name "*.fits" -mtime +120 -delete
 
-# tesscut (from Eleanor)
-find tesscut -type f -name "*.fits" -mtime +30 -delete
+# tesscut (from Eleanor, raw TessCut,  etc)
+find tesscut -type f -name "*.fits" -mtime +14 -delete
 
-# Consider to delete targetpixelfiles more aggressively
-# find . -type f -name "*_tp.fits" -mtime +30 -delete
-
+# delete targetpixelfiles more aggressively
+find . -type f -name "*_tp.fits" -mtime +14 -delete
 
 # Delete download DV files
 cd ~/Downloads
-find . -type f -name "tess*.pdf" -mtime +30 -delete
+find . -type f -name "tess*.pdf" -mtime +2 -delete
 cd -
+
+echo "Disk usage after cleanup"
+df -h .
+du -h -s mastDownload tesscut .
