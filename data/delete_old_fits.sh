dec43a4608f250435c0a13fd023d3885dc65a1a1
orionlee <orionlee@yahoo.com>
2025-05-06 08:24:26 -0700

    delete_old_fits.sh - add more sources

--- data/delete_old_fits.sh
+++ data/delete_old_fits.sh
@@ -17,6 +17,18 @@ find tesscut -type f -name "*.fits" -mtime +14 -delete
 # delete targetpixelfiles more aggressively
 find . -type f -name "*_tp.fits" -mtime +14 -delete
 
+# aggressively deletes the tesscut fits and the processed pickle dumps there
+#   (they're huge)
+cd /c/dev/_juypter/transit-APP/example_output/
+find . -type f -name "*.fits" -mtime +2 -delete 
+find . -type f -name "*.pickle" -mtime +2 -delete
+cd - 
+
+# aggressively deletes these copies (they're nost liked copied from .)
+cd /c/dev/_juypter/TESSPositionalProbability/Lightcurves/
+find . -type f -name "*.fits" -mtime +2 -delete 
+cd -
+
 # Delete download DV files
 cd ~/Downloads
 find . -type f -name "tess*.pdf" -mtime +2 -delete
