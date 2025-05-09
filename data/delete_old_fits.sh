f91c950e82d690e533cac4e6a94864b2056a4cf6
orionlee <orionlee@yahoo.com>
2025-05-09 15:47:37 -0700

    delete_old_fits helper: also delete vetting files from Pyaneti, LATTE,

--- data/delete_old_fits.sh
+++ data/delete_old_fits.sh
@@ -34,6 +34,25 @@ cd ~/Downloads
 find . -type f -name "tess*.pdf" -mtime +2 -delete
 cd -
 
+# Delete old Pyaneti reports
+echo "Manually delete old Pyaneti reports at:"
+echo "/c/dev/_juypter/LATTE/dataLATTE/"
+ls -lGt /c/dev/_juypter/LATTE/dataLATTE/ | head
+
+
+# Delete old large Pyaneti files
+cd /l/home/orionlee/dev/pyaneti/outpy 
+find . -type f -name "*_all_data.dat"  -mtime +120 -delete
+find . -type f -name "*_chains.pdf"   -mtime +120 -delete
+find . -type f -name "*_chains.png"  -mtime +120 -delete
+find . -type f -name "*-tr*_lightcurve.txt"  -mtime +120 -delete
+find . -type f -name "*_correlations.png"  -mtime +120 -delete
+cd -
+
+# OPEN: consider removing (at least the large files)
+# /l/home/orionlee/dev/triceratops/{vetting,misc_targets}/
+
+
 echo "Disk usage after cleanup"
 df -h .
 du -h -s mastDownload tesscut .
