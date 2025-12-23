01e8b5e2d7f1078461438070c8dde206a65f4b88
orionlee <orionlee@yahoo.com>
2025-12-23 09:22:31 -0800

    delete_old_fits.sh : reduce FITS file retention time window

--- data/delete_old_fits.sh
+++ data/delete_old_fits.sh
@@ -13,27 +13,33 @@ echo "Disk usage before cleanup"
 df -h .
 du -h -s mastDownload tesscut .
 
-# Ideally want touch use -atime (last access time), 
-# insetad of -mtime  (modified time)
+# Ideally want touch use -atime (last access time),
+# instead of -mtime  (modified time)
 # but -atime does not seem to work (no file selected)
-find . -type f -name "*.fits" -mtime +120 -delete
+find . -type f -name "*.fits" -mtime +30 -delete
 
 # tesscut (from Eleanor, raw TessCut,  etc)
-find tesscut -type f -name "*.fits" -mtime +14 -delete
+find tesscut -type f -name "*.fits" -mtime +7 -delete
 
 # delete targetpixelfiles more aggressively
-find . -type f -name "*_tp.fits" -mtime +14 -delete
+find . -type f -name "*_tp.fits" -mtime +7 -delete
+
+# remove TCE DV XMLs (shouldn't have them anymore but just in case)
+find mastDownload -type f -name "*_dvr.xml" -mtime +30 -delete
+
+# remove empty directories (for organization)
+find mastDownload -empty -type d -delete
 
 # aggressively deletes the tesscut fits and the processed pickle dumps there
 #   (they're huge)
 cd /c/dev/_juypter/transit-APP/example_output/
-find . -type f -name "*.fits" -mtime +2 -delete 
+find . -type f -name "*.fits" -mtime +2 -delete
 find . -type f -name "*.pickle" -mtime +2 -delete
-cd - 
+cd -
 
-# aggressively deletes these copies (they're nost liked copied from .)
+# aggressively deletes these copies (they're most likely copied from .)
 cd /c/dev/_juypter/TESSPositionalProbability/Lightcurves/
-find . -type f -name "*.fits" -mtime +2 -delete 
+find . -type f -name "*.fits" -mtime +2 -delete
 cd -
 
 # Delete download DV files
@@ -41,22 +47,22 @@ cd ~/Downloads
 find . -type f -name "tess*.pdf" -mtime +2 -delete
 cd -
 
-# Delete FITS file in default and legacy lightkurve cache dirs 
+# Delete FITS file in default and legacy lightkurve cache dirs
 # - these dirs are unintentionally that used at times
 du -h -s ~/.lightkurve/cache  ~/.lightkurve-cache
 cd ~/.lightkurve/cache
-find . -type f -name "*.fits" -mtime +2 -delete 
+find . -type f -name "*.fits" -mtime +2 -delete
 cd ~/.lightkurve-cache
-find . -type f -name "*.fits" -mtime +2 -delete 
+find . -type f -name "*.fits" -mtime +2 -delete
 
-# Delete old Pyaneti reports
-echo "Manually delete old Pyaneti reports at:"
+# Delete old LATTE reports
+echo "Manually delete old LATTE reports at:"
 echo "/c/dev/_juypter/LATTE/dataLATTE/"
 ls -lGt /c/dev/_juypter/LATTE/dataLATTE/ | head
 
 
 # Delete old large Pyaneti files
-cd /l/home/orionlee/dev/pyaneti/outpy 
+cd /l/home/orionlee/dev/pyaneti/outpy
 find . -type f -name "*_all_data.dat"  -mtime +120 -delete
 find . -type f -name "*_chains.pdf"   -mtime +120 -delete
 find . -type f -name "*_chains.png"  -mtime +120 -delete
